853e3434a77be8d522b73b03a95f45ee37c66964
Eitz <richard@eitz.me>
2019-05-27 13:48:57 +0200

    Listen on 80 for now

--- app.js
+++ app.js
@@ -14,6 +14,6 @@ app.get('/', function (req, res) {
     </body>`);
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(80, function () {
+  console.log('Example app listening on port 80!');
 });
