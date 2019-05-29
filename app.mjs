b10f38fce3b8a4ea7827a09b178c04fcd03eaafb
Eitz <richard@eitz.me>
2019-05-29 18:02:22 +0200

    Backend: use env variables to enable/disable CORS

--- app.mjs
+++ app.mjs
@@ -5,13 +5,13 @@ import PostController from './lib/controllers/PostController.mjs';
 
 var app = express();
 
-/*
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
-*/
+if (process.env.NODE_ENV === 'development') {
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+}
 
 // backend
 app.use('/api/', [
