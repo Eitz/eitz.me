0812efd72537f2acc99b0b273da5f626939265d2
Richard Eitz <richard@eitz.me>
2019-07-06 16:24:52 +0200

    Change development port to the standard 3000

--- app.mjs
+++ app.mjs
@@ -3,9 +3,11 @@ import express from 'express';
 import BlogController from './backend/lib/controllers/BlogController.mjs';
 import PostController from './backend/lib/controllers/PostController.mjs';
 
+var port = 80;
 var app = express();
 
 if (process.env.NODE_ENV === 'development') {
+  port = 3000;
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -27,4 +29,4 @@ function webAppHandler(req, res) {
   return res.sendFile('index.html', {root: 'frontend/build'});
 }
 
-app.listen(80, () => console.log('eitz.me listening on port 80!'));
+app.listen(port, () => console.log(`eitz.me listening on port ${port}!`));
--- frontend/src/misc/BaseURL.js
+++ frontend/src/misc/BaseURL.js
@@ -1,4 +1,4 @@
 const baseUrl = process.env && process.env.NODE_ENV === 'development'
-    ? 'http://localhost:80'
+    ? 'http://localhost:3000'
     : '';
 export default baseUrl;
